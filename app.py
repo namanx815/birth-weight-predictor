06e6324dd946e0e0ffa19e81630332c16e26633b
namanx815 <141518752+namanx815@users.noreply.github.com>
2025-06-17 13:25:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def get_prediction():
 
     baby_data_cleaned=get_cleaned_data(baby_data)
     baby_df=pd.DataFrame(baby_data_cleaned )
-    with open("model/model.pkl","rb") as obj:
+    with open("model.pkl","rb") as obj:
         model=pickle.load(obj)
     prediction=model.predict(baby_df)
     prediction=round(float(prediction),2)
